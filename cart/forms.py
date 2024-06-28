22fe353dc82bfe40b502d3495063890380dba1c7
Alexander Belitskiy <alexbel@MacBook-Air-Alexander.local>
2024-06-28 12:58:14 +0200

    cart add product form widget customised

--- cart/forms.py
+++ cart/forms.py
@@ -4,5 +4,6 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1,21)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
-    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int, label='', widget=forms.Select(attrs={"class": "form-control text-center", "style":"max-width: 3rem; border-color: black;"}))
+    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput())
+    
