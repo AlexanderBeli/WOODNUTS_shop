977aa0954539207d5f0410aad47d501d46a59aa2
Alexander <alexander@MacBook-Pro-Alexander.local>
2024-08-17 16:05:06 +0200

    translation added, some files fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,9 +11,5 @@ urlpatterns = [
     path("signup/", SignupPageView.as_view(), name="signup"),
     path("info/<int:pk>/", UserPageDetailView.as_view(), name="userinfo"),
     path("info/<int:pk>/edit/", UserUpdateView.as_view(), name="userinfoedit"),
-    path(
-        "info/<int:pk>/editpassword/",
-        ChangePasswordView.as_view(),
-        name="userinfoeditpw",
-    ),
+    path("info/<int:pk>/editpassword/", ChangePasswordView.as_view(), name="editpw"),
 ]
