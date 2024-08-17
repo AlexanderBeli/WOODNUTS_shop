61668d3c32046b81620852d5255dd0cf3b6fd86c
Alexander <alexander@MacBook-Pro-Alexander.local>
2024-08-17 15:24:14 +0200

    smtp added

--- django_project/settings.py
+++ django_project/settings.py
@@ -224,7 +224,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 CART_SESSION_ID = "cart"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = env.str("EMAIL_DEFAULT_FROM_EMAIL")
+EMAIL_HOST = env.str("EMAIL_EMAIL_HOST")
+EMAIL_HOST_USER = env.str("EMAIL_EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 CELERY_BACKEND = "rpc://"
